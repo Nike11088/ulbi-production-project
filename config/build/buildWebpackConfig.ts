df17c6a8a60da210f429fd37a571d1307b2d7099
Nikita Abakumov <abakumov-n@mail.ru>
2025-02-13 18:33:18 +0300

    init, add webpackconfig

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,26 @@
+import { Configuration } from 'webpack'
+import { BuildOptions } from './types/config'
+import path from 'path'
+import webpack from 'webpack'
+import { buildPlugins } from './buildPlugins'
+import { buildLoaders } from './buildLoader'
+import { buildResolvers } from './buildResolvers'
+
+export function buildWebpackConfig(options: BuildOptions): Configuration {
+  const { paths, mode } = options
+
+  return {
+    mode,
+    entry: paths.entry,
+    output: {
+      filename: '[name].[contenthash].js',
+      path: paths.build,
+      clean: true
+    },
+    plugins: buildPlugins(options),
+    module: {
+      rules: buildLoaders(),
+    },
+    resolve: buildResolvers(),
+  }
+}
