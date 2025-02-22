8c3d517ad3672da6b4a9edb945673cbb80d263da
Nikita Abakumov <abakumov-n@mail.ru>
2025-02-22 22:14:40 +0300

    add jest, unit tests

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,46 +1,54 @@
-import globals from "globals"
-import pluginJs from "@eslint/js"
-import tseslint from "typescript-eslint"
-import pluginReact from "eslint-plugin-react"
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
 import i18next from 'eslint-plugin-i18next'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   i18next.configs['flat/recommended'],
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { languageOptions: { globals: globals.browser } },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.jest
+      }
+    }
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
     rules: {
-      "eol-last": ["error", "always"],
-      "object-curly-spacing": ["error", "always"],
-      "indent": ["error", 2],
-      "semi": ["error", "never"],
-      "no-unused-vars": "warn",
-      "no-undef": "warn",
-      "react/jsx-uses-react": "off",
-      "react/react-in-jsx-scope": "off",
-      "@typescript-eslint/no-unused-vars": "warn",
-      "react/no-deprecated": "off"
+      'quotes': ['error', 'single'],
+      'eol-last': ['error', 'always'],
+      'object-curly-spacing': ['error', 'always'],
+      'indent': ['error', 2],
+      'semi': ['error', 'never'],
+      'no-unused-vars': 'warn',
+      'no-undef': 'warn',
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'react/no-deprecated': 'off'
     },
   },
   {
     ignores: [
-      "cypress",
-      "node_modules",
-      "public",
-      "src/assets",
-      ".gitignore",
-      ".prettierrc",
-      ".prettierignore",
-      "cypress.config.js",
-      "eslint.config.mjs",
+      'cypress',
+      'node_modules',
+      'public',
+      'src/assets',
+      '.gitignore',
+      '.prettierrc',
+      '.prettierignore',
+      'cypress.config.js',
+      'eslint.config.mjs',
     ],
     settings: {
       react: {
-        version: "detect",
+        version: 'detect',
       },
     },
   },
